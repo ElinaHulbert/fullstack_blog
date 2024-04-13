fa1e2ceacdd9941803136b74d14b67c6eb95c952
ElinaHulbert <elina.hulbert@happens.se>
2024-04-13 14:24:52 +0200

    encrypting data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import jsonwebtoken from "jsonwebtoken";
 
 const app = express();
 
@@ -12,8 +13,18 @@ app.get("/", (req, res) => {
 //when the post request comes return the response in json format
 app.post("/auth/login", (req, res) => {
   console.log("body", req.body);
+  //when the request comes, generate a token /encrypting this object/
+  const token = jsonwebtoken.sign(
+    {
+      email: req.body.email,
+      fullName: "Name Surname",
+    },
+    "encryptcode"
+  );
+  //returning the json file with the token to the user
   res.json({
     success: true,
+    token,
   });
 });
 
